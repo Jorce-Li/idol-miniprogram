d8f33f5fe3bcdf77294c5e999243011299283d3f
jorce <17802926729@163.com>
2020-03-12 22:01:22 +0800

    update merge

--- miniprogram/pages/homepage/index.js
+++ miniprogram/pages/homepage/index.js
@@ -14,61 +14,10 @@ Page({
     recoding: false,
     currentSelectedDate: {}
   },
-<<<<<<< HEAD
   getdate:function (e) {
     console.log(e);
     this.setData({
       currentSelectedDate: e.detail
-=======
-  startRecord() {
-    console.log(1111);
-    wx.getSetting({
-      success: res => {
-        if (!res.authSetting['scope.record']) {
-          wx.authorize({
-            scope: 'scope.record',
-            success () {
-              console.log(111111);
-              // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
-              const recoder = wx.getRecorderManager();
-              recoder.start({
-                duration: 2000,
-              });
-              this.setData({
-                recoding: true
-              })
-              recoder.onStop(res => {
-                this.setData({
-                  filePath: res.tempFilePath,
-                  recoding: false
-                })
-              });
-            },
-            fail (err) {
-              wx.showToast({
-                title: "为了您更好的体验,拒绝后请自行在设置中打开麦克风权限",
-                icon: 'none',
-                duration: 2000
-              });
-            }
-          })
-        } else {
-          const recoder = wx.getRecorderManager();
-          recoder.start({
-            duration: 2000,
-          });
-          this.setData({
-            recoding: true
-          })
-          recoder.onStop(res => {
-            this.setData({
-              filePath: res.tempFilePath,
-              recoding: false
-            })
-          });
-        }
-      }
->>>>>>> a749bb1b317eb53cdebdaf9e99f2b186af439e46
     })
     // console.log(e)
   },
